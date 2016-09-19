169df5bcfe642ed335a802444041e5e1358e06b1
zzj0402 <zzj0402@gmail.com>
2016-09-19 17:50:36 -0400

    rot13.py
    Console IO version

---
+++ rot13.py
@@ -0,0 +1,6 @@
+import string
+rot13=string.maketrans(
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+input=raw_input('Enter your input:')
+print string.translate(input,rot13)
